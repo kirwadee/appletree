6151b74a122b771ae34b85d4269268760cd891ad
kirwadee <meshkirwade@gmail.com>
2023-06-16 16:33:55 +0300

    added createSchoolHandler and showSchoolHandler

---
+++ cmd/api/routes.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) routes() *httprouter.Router {
+	//create a new httprouter instance
+	router := httprouter.New()
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/schools", app.createSchoolHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/schools/:id", app.showSchoolHandler)
+
+	return router
+}
