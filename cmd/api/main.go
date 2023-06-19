b45e1c43d9a0bc5a077936bbcdd7d3da6bf58fcc
kirwadee <meshkirwade@gmail.com>
2023-06-19 18:23:50 +0300

    set up database connection pool

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,21 +1,35 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 // application version number
-const version = "1.0.0"
+const (
+	version  = "1.0.0"
+	host     = "localhost"
+	port     = 5432
+	user     = "appletree"
+	password = "$Appletree2022$"
+	dbname   = "appletree"
+)
 
 // The configuration settings
 type config struct {
 	port int
 	env  string //development, production, staging
+	db   struct {
+		dsn string
+	}
 }
 
 // Dependency Injection
@@ -25,14 +39,26 @@ type application struct {
 }
 
 func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
 	var cfg config
 	//read in flags that are needed to populate config struct
 	flag.IntVar(&cfg.port, "port", 4000, "API Server Port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment(development | staging | production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", psqlInfo, "Postgresql dsn")
 	flag.Parse()
 
 	//Create a customized logger
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	//create a connection pool
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	logger.Println("Connected to postgres db")
+	defer db.Close()
 
 	//Create an instance of application struct
 	app := &application{
@@ -54,6 +80,23 @@ func main() {
 	}
 	//start our server
 	logger.Printf("Starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+// The openDB() returns pointer to *sql.DB connection pool
+func openDB(cf config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cf.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	//create a context with 5 seconds timeout deadline
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	//check if the connection to db is still alive
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
